269e28cb02cea69a1ded56792c77cc59ae0c1b79
abhinavkumarsph <abhinavkumar@sph.com.sg>
2023-05-05 10:37:17 +0800

    fix: ism polcies map not sensistive

--- modules/resource-mgt/variables.tf
+++ modules/resource-mgt/variables.tf
@@ -4,6 +4,7 @@ variable "domain_endpoint" {
 }
 
 variable "ism_policies" {
+  sensitive   = false
   description = "A map of all ISM policies to create. Value should be json encoded"
   type        = map(string)
   default     = {}

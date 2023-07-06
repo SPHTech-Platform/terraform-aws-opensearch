702b7e9d5745a32f71f0b5b9d4c8e4b6066726d8
abhinavkumarsph <abhinavkumar@sph.com.sg>
2023-07-06 11:00:22 +0800

    fix: remove non sensitive func for ism

--- modules/resource-mgt/ism.tf
+++ modules/resource-mgt/ism.tf
@@ -1,12 +1,12 @@
 resource "elasticsearch_opensearch_ism_policy" "this" {
-  for_each = nonsensitive(local.new_ism_policies)
+  for_each = local.new_ism_policies
 
   policy_id = each.key
   body      = each.value.body
 }
 
 resource "elasticsearch_opensearch_ism_policy_mapping" "new_policies" {
-  for_each = nonsensitive(local.new_ism_policies)
+  for_each = local.new_ism_policies
 
   policy_id = each.key
   indexes   = each.value.ism_index_pattern
@@ -17,14 +17,14 @@ resource "elasticsearch_opensearch_ism_policy_mapping" "new_policies" {
 }
 
 resource "elasticsearch_opensearch_ism_policy_mapping" "existing_policies" {
-  for_each = nonsensitive(local.existing_ism_policies)
+  for_each = local.existing_ism_policies
 
   policy_id = each.key
   indexes   = each.value.ism_index_pattern
 }
 
 resource "elasticsearch_index_template" "this" {
-  for_each = nonsensitive(var.ism_templates)
+  for_each = var.ism_templates
 
   name = each.key
   body = each.value
